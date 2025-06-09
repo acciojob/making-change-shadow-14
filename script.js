ed09b65c3ec68a7bfe7187e2986cad2c75a66895
Sarthak Goyal <82680055+shadow-14@users.noreply.github.com>
2025-06-09 08:28:58 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,7 +1,23 @@
 const makeChange = (c) => {
   // your name here
+	let ans = {
+		"q":0,
+		"d":0,
+		"n":0,
+		"p":0
+	}
+	ans["q"]=Math.floor(c/25);
+	c=c%25;
+	ans["d"]=Math.floor(c/10);
+	c=c%10;
+	ans["n"]=Math.floor(c/5);
+	c=c%5;
+	ans["p"]=Math.floor(c);
+	
+	return ans;
+	
 };
 
 // Do not the change the code below
-const c = prompt("Enter c: ");
-alert(JSON.stringify(makeChange(c)));
+// const c = prompt("Enter c: ");
+// alert(JSON.stringify(makeChange(c)));
